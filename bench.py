8a68d926846855c31034a38338192335414a9e1c
Michael Goin <michael@neuralmagic.com>
2023-09-05 16:14:18 -0400

    Update bench.py

--- bench.py
+++ bench.py
@@ -3,6 +3,7 @@ import json
 
 from deepsparse.benchmark.benchmark_sweep import benchmark_sweep
 
+# Set up the 
 models = [
     "zoo:cv/classification/mobilenet_v1-1.0/pytorch/sparseml/imagenet/pruned_quant-moderate",
     "zoo:cv/detection/yolov5-n/pytorch/ultralytics/coco/base_quant-none",
@@ -14,6 +15,7 @@ export_csv_path = "benchmark_sweep.csv"
 # Run the benchmarks
 benchmark_sweep(models, batch_sizes=batch_sizes, export_csv_path=export_csv_path)
 
+# Read the benchmarks CSV output and convert it to the JSON format for github-action-benchmark
 data = []
 with open(export_csv_path, newline="") as csvfile:
     reader = csv.DictReader(csvfile)
@@ -29,10 +31,11 @@ with open(export_csv_path, newline="") as csvfile:
                 "name": f"{model_name}, batch_size:{batch_size}",
                 "value": float(items_per_second),
                 "unit": "items/sec",
-                "range": f"{1000/float(latency_std)}",
-                "extra": f"mean: {latency_mean}ms\nmedian: {latency_median}ms\nstd: {latency_std}ms"
+                "range": f"{latency_std}",
+                "extra": f"mean: {latency_mean} ms\nmedian: {latency_median} ms\nstd: {latency_std} ms"
             }
         )
 
+# Write out the final JSON for github-action-benchmark
 with open("output.json", "w") as output:
     json.dump(data, output)
