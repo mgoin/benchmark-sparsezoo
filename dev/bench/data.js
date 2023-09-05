e39cefaa0f0bf10e5e848ae886bc231bc983613c
github-action-benchmark <github@users.noreply.github.com>
2023-09-05 19:12:28 +0000

    add DeepSparse benchmarks on SparseZoo (customBiggerIsBetter) benchmark result for 243820d1f3328bc535458a484dddd22ced81f676

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693941033320,
+  "lastUpdate": 1693941148517,
   "repoUrl": "https://github.com/mgoin/benchmark-sparsezoo",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -299,6 +299,53 @@ window.BENCHMARK_DATA = {
             "unit": "items/sec",
             "range": "985.9256859526377",
             "extra": "mean: 6.3891690601275215ms\nmedian: 6.124982000002888ms\nstd: 1.0142752280906073ms"
+          }
+        ]
+      }
+    ],
+    "DeepSparse benchmarks on SparseZoo": [
+      {
+        "commit": {
+          "author": {
+            "email": "michael@neuralmagic.com",
+            "name": "Michael Goin",
+            "username": "mgoin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "243820d1f3328bc535458a484dddd22ced81f676",
+          "message": "Update benchmark.yml",
+          "timestamp": "2023-09-05T15:09:50-04:00",
+          "tree_id": "c10259b11409b0447b00df25ee90b37ab508a7be",
+          "url": "https://github.com/mgoin/benchmark-sparsezoo/commit/243820d1f3328bc535458a484dddd22ced81f676"
+        },
+        "date": 1693941147796,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "zoo:cv/classification/mobilenet_v1-1.0/pytorch/sparseml/imagenet/pruned_quant-moderate, batch_size:1",
+            "value": 194.5726275097905,
+            "unit": "items/sec",
+            "range": "1602.0178763585945",
+            "extra": "mean: 5.130373072627637ms\nmedian: 5.038469500007636ms\nstd: 0.624212759893174ms"
+          },
+          {
+            "name": "zoo:cv/detection/yolov5-n/pytorch/ultralytics/coco/base_quant-none, batch_size:1",
+            "value": 43.00698806814199,
+            "unit": "items/sec",
+            "range": "1130.8013731171654",
+            "extra": "mean: 23.23802305189746ms\nmedian: 23.02745099999015ms\nstd: 0.8843286042741543ms"
+          },
+          {
+            "name": "zoo:nlp/multilabel_text_classification/distilbert-none/pytorch/huggingface/goemotions/pruned90_quant-none, batch_size:1",
+            "value": 234.9865310185166,
+            "unit": "items/sec",
+            "range": "2285.801519290351",
+            "extra": "mean: 4.2478059982977925ms\nmedian: 4.135017000010066ms\nstd: 0.43748330358554477ms"
           }
         ]
       }
